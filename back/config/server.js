28c6b6170f31398e96ff8c89edcc8c6b76feafa5
Miguel Rodriguez <m.rodriguezescoll@gmail.com>
2020-10-19 19:22:27 +0200

    yess

---
+++ back/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '127347ed1f6fc156626103bc713d6ae7'),
+    },
+  },
+});
---
+++ back/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e58f0970-7e0a-4324-b0de-926833fb3363'
+};
